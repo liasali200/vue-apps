73ad5106e1d93971d3630c9114a22f00d762a532
Lia S <eopqrs@gmail.com>
2026-03-19 16:29:47 +0000

    added button and card styles

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,25 @@ import Main from "@/components/Main.vue";
 </script>
 
 <template>
- <Header/>
-<Main/>
+  <div id="app-card">
+    <Header/>
+    <Main/>
+    <Footer/>
+  </div>
 
-  <Footer/>
 </template>
 
 <style scoped>
 
-
+#app-card{
+  margin: 3rem;
+  padding: 2rem;
+  border-radius: 1rem;
+  box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.2);
+  display: grid;
+  gap: 1rem;
+  background-color: #cacaca55;
+}
 
 @media (min-width: 1024px) {
 }
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,11 +9,30 @@ const name = ref('Lia')
 
 <template>
     <section>
-        <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes</h4>
+        <h3>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes</h3>
         <button>Next Quote</button>
     </section>
 
 </template>
-2
+
 <style scoped>
+
+h3{
+    color: #f3f9f3;
+    font-weight: 600;
+}
+
+button{
+    background-color: #277429; /* Green */
+    border: none;
+    border-radius: 2em;
+    box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.2);
+    color: white;
+    margin-top: 1rem;
+    padding: 7px 28px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+}
 </style> 
